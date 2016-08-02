85618d4e7cb46f438ffa641b0d4804c1243bf77d
rtx <vafarulez@ya.ru>
2016-08-02 15:08:01 +0300

    все пути в html файле

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,4 @@
 'use strict'
 window.onload = function() {
 
-  var scrollUp = document.getElementById('top');
-
-  scrollUp.onclick = function() {
-		window.scrollTo(0,0);
-    return false;
-	};
-
 };
